f8e466132d5759e4f4d8624a94320474c87f9cba
ErnestMakarov <ernest.makarov@tptlive.ee>
2026-06-15 03:33:32 +0300

    scroll top fix, react router dom bug

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -2,14 +2,19 @@ import { Outlet } from "react-router-dom";
 
 import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
+import ScrollToTop from "../components/ScrollToTop";
 
 export default function App() {
   return (
     <>
+      <ScrollToTop />
+
       <Header />
+
       <main>
         <Outlet />
       </main>
+
       <Footer />
     </>
   );
