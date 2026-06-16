a9453e745945010055bee3aebe9a39a7725cdac8
ErnestMakarov <ernest.makarov@tptlive.ee>
2026-06-16 03:27:40 +0300

    vercel analytic

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -4,9 +4,13 @@ import Header from "../components/Header/Header";
 import Footer from "../components/Footer/Footer";
 import ScrollToTop from "../components/ScrollToTop";
 
+import { Analytics } from "@vercel/analytics/react";
+
 export default function App() {
   return (
     <>
+      <Analytics />
+      
       <ScrollToTop />
 
       <Header />
